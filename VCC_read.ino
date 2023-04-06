f5f4dce8ff9c78abd60acf244c7f4237cf150d94
Hans Crijns <hanscrijns@yahoo.com>
2023-04-06 15:59:54 +0200

    Added ino

---
+++ VCC_read.ino
@@ -0,0 +1,25 @@
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  Serial.println(readVcc());
+  delay(500);        // delay in between reads for stability
+}
